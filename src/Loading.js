6c2d81d1d2b0a4165f6cc93bc521038ce21ec055
HagiaSofiya <SofiyaShrayber@gmail.com>
2019-03-06 00:14:49 -0500

    [style] Loading animation

--- src/Loading.js
+++ src/Loading.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import loading from './misc/loading.png';
+import { LoadingStyle, LoadingImg } from './Styles';
 
 const Loading = () => (
-	<div>Loading</div>
+	<div style={LoadingStyle}>
+		<img src={loading} style={LoadingImg} alt='loading' />
+	</div>
 )
   
 export default Loading;
---
+++ src/Styles.js
@@ -0,0 +1,15 @@
+const LoadingStyle = {
+    "alignItems": "center",
+    "display": "flex",
+    "justifyContent": "center",
+    "width": "100vw",
+    "height": "100vh"
+}
+
+const LoadingImg = {
+    "animation": "loading-spin infinite 20s linear",
+    "height": "40vmin",
+    "pointerEvents": "none"
+}
+
+export { LoadingStyle, LoadingImg }
